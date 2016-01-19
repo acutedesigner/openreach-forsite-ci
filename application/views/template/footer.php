099e5da43d9ebb16dc357b1d243c8dd7ae6d0fe4
Nigel M Peters <develop@acumendesign.co.uk>
2016-01-19 17:26:34 +0000

    Updates to the foot

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,8 +1,8 @@
-			</section>									
-	
+			</section>
+
 			<div class="push"></div>
 		</div>
-		
+
 		<footer>
 			<div class="container">
 				<ul>
@@ -14,19 +14,19 @@
 					<li><a href="mailto:customer-feedback@openreach.co.uk" target="_blank">Contact us</a></li>
 					<?php if(isset($footer_menu)): ?>
 						<?php foreach ($footer_menu as $result): ?>
-						
+
 							<li><a href="<?php echo base_url($result->friendly_title); ?>"><?php echo $result->title; ?></a></li>
-						
+
 						<?php endforeach; ?>
 					<?php endif; ?>
-				</ul>				
+				</ul>
 			</div>
 			<div class="container copyright">
-				<p>&copy; British Telecommunications plc <?php echo date('YY'); ?>. All rights reserved.</p>
+				<p>&copy; British Telecommunications plc <?php echo date('Y'); ?>. All rights reserved.</p>
 			</div>
 		</footer>
-	
-	
+
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="<?php echo $this->template_url; ?>js/plugins.js"></script>
         <script src="<?php echo $this->template_url; ?>js/main.js"></script>
@@ -34,10 +34,10 @@
         <script src="<?php echo $this->template_url; ?>js/vendor/jquery.cookiecuttr.js"></script>
         <link rel="stylesheet" href="<?php echo $this->template_url; ?>js/vendor/cookiecuttr.css" title="Cookie Cutter" type="text/css" media="screen" charset="utf-8">
 		<script>
-						
+
 		// You will need this for the menu toggle
 		jQuery(document).ready(function($){
-		
+
 			$.cookieCuttr({
 				cookieAnalytics: false,
 				cookiePolicyLink: "/cookie-policy/",
@@ -45,16 +45,16 @@
 				cookieMessage: 'We use cookies on this website, you can <a href="{{cookiePolicyLink}}" title="read about our cookies">read about them here</a>.',
 				cookieAcceptButtonText: "CONTINUE"
 			});
-		
+
 			//------ MAIN MENU ------//
 			$('.to-main-nav').click(function(e) {
 				e.preventDefault();
 				$('.menu-container ul').slideToggle();
 			});
-		
+
 		});
-		
-		</script>        
+
+		</script>
 
     </body>
 </html>
